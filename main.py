a5cff8d5edb335f77fdce00ef60b10ce70f73409
Chris Griffith <chris@cdgriffith.com>
2018-09-17 16:11:44 -0500

    built in legacy font?

--- main.py
+++ main.py
@@ -5,26 +5,29 @@ import os
 from threading import Thread, Lock
 
 from pynput import keyboard
-from PIL import ImageFont
+# from PIL import ImageFont
 # from luma.core.interface.serial import spi, noop
 # from luma.core.render import canvas
 # from luma.led_matrix.device import max7219
+# from luma.core.legacy.font import proportional, LCD_FONT
 #
 # serial = spi(port=0, device=0, gpio=noop())
-# device = max7219(serial, cascaded=5, block_orientation=0) # may be 90 or -90
+# device = max7219(serial, cascaded=4, block_orientation=0) # may be 90 or -90
 #
 # font_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'fonts', 'lato' 'Lato-Regular.ttf'))
 # font = ImageFont.truetype(font_path, 12)
-
+# font = proportional(LCD_FONT)
 
 action_lock = Lock()
 focus = False
 timer = 10.1
 capture_mode = False
 
+
 def display(text):
     with canvas(device) as draw:
         draw.text((0, 0), text, font=font, fill="white")
+
 
 def toggle_focus():
     global focus
